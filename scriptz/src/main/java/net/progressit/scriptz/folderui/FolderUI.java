4a3862f07aff0f66e33d8f758daa82805604be3f
Theodore R <theomails@gmail.com>
2021-09-20 22:40:15 +0530

    Removed the copies kept here, and referred Folderzui project.

--- scriptz/src/main/java/net/progressit/scriptz/folderui/FolderUI.java
+++ scriptz/src/main/java/net/progressit/scriptz/folderui/FolderUI.java
@@ -1,128 +1,31 @@
 package net.progressit.scriptz.folderui;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.event.MouseWheelListener;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Paths;
 
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JFileChooser;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
 
-import com.google.common.eventbus.EventBus;
-
-import net.miginfocom.swing.MigLayout;
+import net.progressit.folderzui.ui.VisualizeFolderApp;
+import net.progressit.progressive.PComponent;
+import net.progressit.progressive.PComponent.PEventListener;
+import net.progressit.progressive.PComponent.PPlacers;
 import net.progressit.scriptz.core.ScriptInternalFrame;
-import net.progressit.scriptz.folderui.Scanner.FolderDetails;
 
 public class FolderUI  extends  ScriptInternalFrame{
 	private static final long serialVersionUID = 1L;
 
 	public FolderUI() {
-		super("Folder UI", true, true, true, true);
+		super("Visualize Folder", true, true, true, true);
 	}
 	
-	//Interal bus for this script.
-	private final EventBus bus = new EventBus();
-	private final Scanner scanner = new Scanner();
-	
-	private DrawPanel drawPanel = new DrawPanel();
-	private JScrollPane spDrawPanel = new JScrollPane(drawPanel);
-	private JPanel pnlBrowse = new JPanel(new MigLayout("insets 0", "[][grow, fill][][][]", "[]"));
-	private JTextField tfPath = new JTextField();
-	private JButton btnBrowse = new JButton("Browse...");
-	private JCheckBox cbCountDisplay = new JCheckBox("Show No. of Files", false);
-	private JButton btnScan = new JButton("Scan");
-	private JPanel pnlStatus = new JPanel(new MigLayout("insets 0", "[grow, fill]", "[]"));
-	private JLabel lblStatus = new JLabel("Ready.");
+	private String home = System.getProperty("user.home");
+	private PPlacers simplePlacers = new PPlacers( (component)->getContentPane().add(component, BorderLayout.CENTER), (component)->getContentPane().remove(component) );
+	private VisualizeFolderApp app = new VisualizeFolderApp(simplePlacers, this);
+
 	public void init() {
-		add(spDrawPanel, BorderLayout.CENTER);
-		add(pnlBrowse, BorderLayout.NORTH);
-		add(pnlStatus, BorderLayout.SOUTH);
-
-		pnlBrowse.add(new JLabel("Folder to scan"));
-		pnlBrowse.add(tfPath);
-		pnlBrowse.add(btnBrowse);
-		pnlBrowse.add(cbCountDisplay);
-		pnlBrowse.add(btnScan);
+		PComponent.place(app, new PEventListener() {}, home);
 		
-		pnlStatus.add(lblStatus);
-
-		tfPath.setText(System.getProperty("user.home"));
-
-		addHandlers();
-
-	}
-
-	private void addHandlers() {
-		MouseWheelListener[] listeners = spDrawPanel.getMouseWheelListeners();
-		for (MouseWheelListener lis : listeners) {
-			spDrawPanel.removeMouseWheelListener(lis);
-		}
-
-		spDrawPanel.addMouseWheelListener((e) -> {
-			//System.out.println(e);
-			if (e.isControlDown()) {
-				int rotation = -1 * e.getWheelRotation();
-				rotation %= 3;
-
-				double scale = (5d + dbl(rotation)) / 5d;
-				int width = (int) (dbl(drawPanel.getWidth()) * scale);
-				int height = (int) (dbl(drawPanel.getWidth()) * scale);
-				width = (width < spDrawPanel.getWidth()) ? spDrawPanel.getWidth() : width;
-				height = (height < spDrawPanel.getHeight()) ? spDrawPanel.getHeight() : height;
-				drawPanel.setPreferredSize(new Dimension(width, height));
-				drawPanel.setSize(new Dimension(width, height));
-				drawPanel.repaint();
-			} else {
-				// pass the event on to the scroll pane
-				// getParent().dispatchEvent(e);
-			}
-		});
-
-		btnBrowse.addActionListener((e) -> {
-			JFileChooser chooser = new JFileChooser();
-			chooser.setCurrentDirectory(new File( tfPath.getText() ));
-			chooser.setDialogTitle("Choose Folder to Scan...");
-			chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-			// disable the "All files" option.
-			chooser.setAcceptAllFileFilterUsed(false);
-			if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-				File result = chooser.getSelectedFile();
-				if(!result.isDirectory()) {
-					result = chooser.getCurrentDirectory();
-				}
-				tfPath.setText( result.toPath().toString() );
-			}
-		});
-		
-		btnScan.addActionListener( (e)->{
-			lblStatus.setText("Scanning...");
-			new Thread( ()->{
-				
-				try {
-					FolderDetails details = scanner.scan(cbCountDisplay.isSelected(), Paths.get(tfPath.getText()) );
-					
-					SwingUtilities.invokeLater( ()->{
-						drawPanel.setDetails(details);
-					} );
-					lblStatus.setText("Ready.");
-				} catch (IOException e1) {
-					lblStatus.setText( e1.toString() );
-				}
-				
-			} ).start();
-		} );
-	}
-
-	private double dbl(long val) {
-		return ((double) val);
+		setSize(800, 600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Visualize Folder");
 	}
 }
